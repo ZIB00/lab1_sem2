2261e70ef9eba095ea612e3154fd69c600e2f688
ZIB00 <alexeypobedaabedoqyexela@gmail.com>
2026-02-25 02:20:55 +0300

    Прошлая версия была не та, я сохранил нерабочий код, исправил, добавил обработку ошибок и начал делать полиморфизм

--- logic.c
+++ logic.c
@@ -1,23 +1,30 @@
-int create_matrix(int size, int type_of_numbers) {
-    
+#include <stdio.h>
+
+typedef struct {
+    void (*create_matrix)(const void*)
+} TypeInfo;
+
+void error(int *variable, int left_boarder, int right_boarder, char error_text[]) {
+    int c;
+    int var = *variable;
+    while (1) {
+                if (scanf("%d", &var) == 1 && (var >= left_boarder && var <= right_boarder)) {
+                    while ((c = getchar()) != '\n');
+                    break;
+                } else {
+                    while ((c = getchar()) != '\n');
+                    printf("%s", error_text);
+                }
+            }
+    *variable = var;
 }
 
-int matrix_addition() {
-
+void create_matrix_int(int size, const void* num) {
+    printf("%d, %d\n", *(int*)num);
 }
 
-int matrix_multiplication() {
-
-}
-
-int multiplication_by_a_scalar() {
-
-}
-
-int adding_a_linear_combination() {
-
-}int create_matrix(int size, int type_of_numbers) {
-    
+void create_matrix_float(int size, const void* num) {
+    printf("%d, %f\n", *(float*)num);
 }
 
 int matrix_addition() {
--- logic.h
+++ logic.h
@@ -1,12 +1,7 @@
 
-
-int create_matrix(int size, int type_of_numbers);
-int matrix_addition();
-int matrix_multiplication();
-int multiplication_by_a_scalar();
-int adding_a_linear_combination();
-
-int create_matrix(int size, int type_of_numbers);
+void error(int *variable, int left_boarder, int right_boarder, char error_text[]);
+int create_matrix_int(int size, int num);
+int create_matrix_float(int size, float num);
 int matrix_addition();
 int matrix_multiplication();
 int multiplication_by_a_scalar();
